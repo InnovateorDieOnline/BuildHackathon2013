a4fa300874d316861a395ead218dd57c6852227d
sjj <sjj698@zepler.org>
2013-06-26 05:27:39 +0100

    blank

---
+++ LiveCycle/LiveCycle/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using LiveCycle.Resources;
+
+namespace LiveCycle
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
